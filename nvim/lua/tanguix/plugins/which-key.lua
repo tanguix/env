b9e04e4be7dc7669e567f7efa4b475877e19e96e
tanguix <mikiyax09@gmail.com>
2024-07-09 09:35:32 +0800

    reset n to its default behavior in which-key.nvim

--- nvim/lua/tanguix/plugins/which-key.lua
+++ nvim/lua/tanguix/plugins/which-key.lua
@@ -9,8 +9,19 @@ return {
   end,                      -- keymap.set("", "", "", { desc = "" }), desc will show with "which-key.nvim"
 
   opts = {
+    -- Add this configuration to exclude 'n' from which-key
+    triggers_blacklist = {
+      n = { "n" },
+    },
     -- your configuration comes here 
     -- or leave it empty to use the default settings 
     -- refer to the configuration section below
-  }
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    -- Explicitly set 'n' back to its default behavior
+    vim.keymap.set('n', 'n', 'n', { noremap = true, desc = "Next search result" })
+  end
+
 }
